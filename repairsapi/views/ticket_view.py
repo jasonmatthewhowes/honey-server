f4feaedde8ceeec92b018e561ff8a50bcd75259e
Jason Howes <110127469+jasonmatthewhowes@users.noreply.github.com>
2023-02-09 14:14:58 -0600

    Added destroy, put

--- repairsapi/views/ticket_view.py
+++ repairsapi/views/ticket_view.py
@@ -4,10 +4,50 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
 from repairsapi.models import ServiceTicket
+from repairsapi.models import Employee
+from repairsapi.models import Customer
 
 
 class TicketView(ViewSet):
     """Honey Rae API customers view"""
+
+    def create(self, request):
+        """Handle POST requests for service tickets
+
+        Returns:
+            Response: JSON serialized representation of newly created service ticket
+        """
+        new_ticket = ServiceTicket()
+        new_ticket.customer = Customer.objects.get(user=request.auth.user)
+        new_ticket.description = request.data['description']
+        new_ticket.emergency = request.data['emergency']
+        new_ticket.save()
+
+        serialized = TicketSerializer(new_ticket, many=False)
+
+        return Response(serialized.data, status=status.HTTP_201_CREATED)
+
+    def update (self, request, pk=None):
+        #Select the targeted ticket using pk
+         
+    #get the employee id from the client request
+        employee_id = request.data['employee']
+        #assign that employee from the database using that id
+        assigned_employee = Employee.objects.get(pk=employee_id)
+#assign that employee instance to the employee property of the ticket
+        ticket.employee = assigned_employee
+#save the updated ticket
+        ticket.save()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+
+    def destroy(self, request, pk=None):
+
+    #handle delete requests for service tickets returns response NONE with 204 status code
+        ticket = ServiceTicket.objects.get(pk=pk)
+        ticket.delete()
+        return Response(None , status=status.HTTP_204_NO_CONTENT)
+
 
     def list(self, request):
         """Handle GET requests to get all customers
@@ -15,8 +55,16 @@ class TicketView(ViewSet):
         Returns:
             Response -- JSON serialized list of customers
         """
-        if request.auth.user.is_staff:    
+
+        tickets = []
+        if request.auth.user.is_staff:
             tickets = ServiceTicket.objects.all()
+            if "status" in request.query_params:
+                    if request.query_params['status'] == "done":
+                        tickets = tickets.filter(date_completed__isnull=False)
+                        
+                    if request.query_params['status'] == "all":
+                        pass
         else:
             tickets = ServiceTicket.objects.filter(customer__user = request.auth.user)
         serialized = TicketSerializer(tickets, many=True)
@@ -33,9 +81,23 @@ class TicketView(ViewSet):
         serialized = TicketSerializer(ticket, context={'request': request})
         return Response(serialized.data, status=status.HTTP_200_OK)
 
+class TicketEmployeeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Employee
+        fields = ('id', 'full_name', 'specialty')
+
+
+class TicketCustomerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Customer
+        fields = ('id', 'user','full_name', 'address')
 
 class TicketSerializer(serializers.ModelSerializer):
     """JSON serializer for tickets"""
+    employee = TicketEmployeeSerializer(many=False)
+    customer = TicketCustomerSerializer(many=False)
     class Meta:
         model = ServiceTicket
         fields = ('id', 'description', 'emergency', 'date_completed', 'employee', 'customer', )
